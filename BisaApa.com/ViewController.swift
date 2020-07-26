6447720ba2e14fbfda255935ed5c74dae80f9da2
Stendy Antonio <stendy20.sa@gmail.com>
2020-07-26 16:23:10 +0700

    Initial Commit

---
+++ BisaApa.com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BisaApa.com
+//
+//  Created by Stendy Antonio on 26/07/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
